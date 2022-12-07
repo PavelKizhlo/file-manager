74c7fe98e6613c868177b5ec1359fba5c32667d9
Pavel Kizhlo <pavelkizh@gmail.com>
2022-12-07 21:13:35 +0300

    feat: add basic structure, implement user greeting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('File manager')
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import { homedir } from 'os';
+import * as color from './libs/color/color.js'
+
+function startFileManager () {
+  const args = process.argv.slice(2);
+  const userNameArg = args.find(arg => arg.match(/^--username/));
+
+  const userName = userNameArg ?
+      userNameArg.replace('--username=', '').trim() :
+      'Anonymous';
+
+  let pwd = homedir();
+
+  const rl = readline.createInterface({input, output});
+
+  console.log(color.green(`Welcome to the File Manager, ${userName}!`))
+  console.log(color.yellow(`You are currently in ${pwd}`))
+
+  rl.on('close', () => {
+    console.log(color.green(`Thank you for using File Manager, ${userName}, goodbye!`))
+  })
+}
+
+startFileManager();
---
+++ src/libs/color/color.js
@@ -0,0 +1,11 @@
+export function green(str) {
+  return `\x1b[32m${str}\x1b[0m`;
+}
+
+export function yellow(str) {
+  return `\x1b[33m${str}\x1b[0m`;
+}
+
+export function red(str) {
+  return `\x1b[31m${str}\x1b[0m`;
+}
