feff04378d2e275efdef52f6871468cce3d58422
Pavel Kizhlo <pavelkizh@gmail.com>
2022-12-12 16:38:49 +0300

    fix: fix issue with renaming a file with an already existing name

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -2,6 +2,7 @@ import { rename } from 'fs/promises'
 import { join, parse } from 'path';
 import { green, red } from '../libs/color/color.js';
 import { resolveFilePath } from '../libs/pathCheck/resolveFilePath.js';
+import { checkPathExists } from '../libs/pathCheck/checkPathExists.js';
 
 export async function renameFile(pwd, args) {
   try {
@@ -17,6 +18,14 @@ export async function renameFile(pwd, args) {
     }
 
     const newPath = join(parse(oldPath).dir, newFilename);
+
+    const newPathExists = await checkPathExists(newPath);
+
+    if (newPathExists) {
+      console.log(red('Invalid input. File already exists'));
+      return;
+    }
+
     await rename(oldPath, newPath);
 
     console.log(green('File was renamed'));
